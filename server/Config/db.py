4297f6bb7c341bca56741306cb71f0cebd6cafec
laavanjan <130158996+laavanjan@users.noreply.github.com>
2025-08-13 16:42:45 +0530

    removed pymongo and added motor

--- server/Config/db.py
+++ server/Config/db.py
@@ -1,6 +1,4 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo import ReturnDocument
-from fastapi import HTTPException
 import logging
 
 URL = "mongodb+srv://sinthujan:Pwd2002@rootcodeproject.buidvwn.mongodb.net/"
@@ -12,9 +10,9 @@ try:
     client = AsyncIOMotorClient(URL)
     db = client["rootcodeproject"]
     logger.info("Connected to MongoDB successfully.")
-    warrent_collection = db.getCollection("Warrent")
-    salary_particular_collection = db.getCollection("Salary_Particular")
+    warrent_collection = db["Warrent"]
+    salary_particular_collection = db["Salary_Particular"]
     logger.info("Collections initialized successfully.")
 except Exception as e:
     logger.error(f"Failed to connect to MongoDB: {e}")
-    raise HTTPException(status_code=500, detail="Database connection error")
+    raise RuntimeError("Database connection error")
