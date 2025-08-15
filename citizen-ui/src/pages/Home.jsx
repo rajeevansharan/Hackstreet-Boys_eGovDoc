02a3352590fa7ed29d887176a9419ca8ffb06d55
Lakshan1806 <lakshanuthayan@gmail.com>
2025-08-15 18:16:49 +0530

    chore: Remove unused PrimaryNavBar imports from pages
    Cleaned up AccountPage, NotificationsPage, and RequestPage by removing unused imports of PrimaryNavBar. Also replaced HomePage with RouteSelect in Home.jsx for improved routing.

--- citizen-ui/src/pages/Home.jsx
+++ citizen-ui/src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import HomePage from "./HomePage";
+import RouteSelect from "../components/RouteSelect";
 import PrimaryNavBar from "../components/PrimaryNavBar";
 
 export default function Home() {
   return (
     <div className="relative flex h-screen flex-col bg-gradient-to-t from-[#3A66A3] from-4% via-[#9DB2D1] via-72% to-[#FFFFFF] to-100%">
-      <HomePage />
+      <RouteSelect />
       <PrimaryNavBar />
     </div>
   );
