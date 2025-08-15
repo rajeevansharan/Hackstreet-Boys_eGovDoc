43bb267897234fd14713186320b20fe0a39e7fe6
rajeevansharan <rajeevansharan@gmail.com>
2025-08-16 00:37:26 +0530

    Added some files

--- citizen-ui/src/routes.jsx
+++ citizen-ui/src/routes.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import TravelWarrantForm from './pages/TravelWarrant'
 
 const router = createBrowserRouter([
   {
     path: "/*",
     element: <Home />,
   },
+  {
+    path: "/travel-warrant",
+    element: <TravelWarrantForm />,
+  }
 ]);
 
 export default router;
