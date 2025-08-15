3d82c63a12423f45c25e833a65b1178313c8f798
Lakshan1806 <lakshanuthayan@gmail.com>
2025-08-15 07:58:08 +0530

    fix: Navigation Bar Alignment

--- citizen-ui/src/components/NavBar.jsx
+++ citizen-ui/src/components/NavBar.jsx
@@ -1,15 +1,28 @@
-import notifications from "../assets/bell.svg";
-import home from "../assets/house-door.svg";
-import requests from "../assets/list-task.svg";
-import user from "../assets/person.svg";
+import { useState } from "react";
+import { IoHomeOutline } from "react-icons/io5";
+import { TfiList } from "react-icons/tfi";
+import { FaRegBell } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa6";
 
 function NavBar() {
   return (
-    <div className="glassy-card flex w-full  justify-between absolute bottom-0 p-[10px] mb-[34px] mx-[10px]">
-      <img width="25px" height="25px" src={home} className="glassy-card"/>
-      <img width="25px" height="25px" src={requests} />
-      <img width="25px" height="25px" src={notifications} />
-      <img width="25px" height="25px" src={user} />
+    <div className="absolute inset-x-[10px] bottom-[34px] grid grid-cols-4 justify-items-center gap-2 rounded-full bg-white/10 px-[10px] py-3 shadow-[inset_0_1px_0_rgba(255,255,255,.45),0_10px_40px_rgba(0,0,0,.25)] ring-1 ring-white/40 backdrop-blur-xl">
+      <div className="flex flex-col items-center text-xs font-bold">
+        <IoHomeOutline className="h-[25px] w-[25px]" />
+        Home
+      </div>
+      <div className="flex flex-col items-center text-xs font-bold">
+        <TfiList className="h-[25px] w-[25px]" />
+        Requests
+      </div>
+      <div className="flex flex-col items-center text-xs font-bold">
+        <FaRegBell className="h-[25px] w-[25px]" />
+        Notifications
+      </div>
+      <div className="flex flex-col items-center text-xs font-bold">
+        <FaRegUser className="h-[25px] w-[25px]" />
+        Account
+      </div>
     </div>
   );
 }
--- citizen-ui/src/pages/Home.jsx
+++ citizen-ui/src/pages/Home.jsx
@@ -2,9 +2,8 @@ import NavBar from "../components/NavBar";
 
 const Home = () => {
   return (
-    <div className="relative h-full bg-gradient-to-t from-[#3A66A3] from-4% via-[#9DB2D1] via-72% to-[#FFFFFF] to-100% flex flex-col items-center justify-center ">
-      
-      <NavBar/>
+    <div className="relative flex h-full flex-col items-center justify-center bg-gradient-to-t from-[#3A66A3] from-4% via-[#9DB2D1] via-72% to-[#FFFFFF] to-100%">
+      <NavBar />
     </div>
   );
 };
